cb8f5202d1ec556099d13844f2e3c21b55c1b05c
Andrew Hill <mdude2314@gmail.com>
2019-10-23 00:56:53 +0000

    I DON'T KNOW WHAT IS WRONG WITH THIS

--- bot.js
+++ bot.js
@@ -1,222 +1,136 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
-const config = require('./config.json');
-const axios = require('axios');
+const config = require("./config.json");
+const axios = require("axios");
+const path = require('path');
 
 let availableSounds = [
-  'go',
-  'go2',
-  'dingus',
-  'fucked',
-  'grin',
-  'gtahorn',
-  'nervous',
-  'quack',
-  'ready',
-  'relax',
-  'slut',
-  'tequila',
-  'turn',
-  'why',
-  'wow',
-  'horn',
-  'alert',
-  'snakelong',
-  'snaake',
-  'realestate',
-  'uganda',
-  'rip',
-  'doubtit'
+  "go",
+  "go2",
+  "dingus",
+  "fucked",
+  "grin",
+  "gtahorn",
+  "nervous",
+  "quack",
+  "ready",
+  "relax",
+  "slut",
+  "tequila",
+  "turn",
+  "why",
+  "wow",
+  "horn",
+  "alert",
+  "snakelong",
+  "snaake",
+  "realestate",
+  "uganda",
+  "rip",
+  "doubtit",
+  "spotted",
+  "coldone",
+  "curb",
+  "dud",
+  "wrekt",
+  "roommate",
+  "yeet",
+  "bruh"
 ];
 
 disconnect = () => {
-  client
-    .voiceConnections
-    .forEach(connection => {
-      connection.disconnect();
-    });
+  client.voiceConnections.forEach(connection => {
+    connection.disconnect();
+  });
 };
 
-exports.sendBitbucketNotification = async bitbucketPayload => {
-  const changeHashes = [],
-    changes = bitbucketPayload.changes,
-    projectSlug = bitbucketPayload.repository.slug,
-    projectKey = bitbucketPayload.repository.project.key;
-  let description = '';
-  changes.forEach(change => {
-    changeHashes.push(change.toHash);
-  });
-
-  let url = `${config.baseApiUrl}projects/${projectKey}/repos/${projectSlug}/commits/`;
-
-  let commitMessages = changeHashes.map(hash => {
-    let response = await axios({
-      method: 'get',
-      url: url + hash,
-      auth: {
-        username: config.apiUser,
-        password: config.apiPass
-      }
-    });
-    return response.data.message;
-  });
-
-  commitMessages.forEach(message => {
-    description += `${message}`;
-    description += '\n';
-  });
-  description += bitbucketPayload.actor.username
-  const url = config.discordBitbucketWebhookUrl;
-
-  const data = {
-    embeds: [
-      {
-        title: `Bitbucket Push`,
-        author: {
-          name: 'BitBucket',
-          icon_url: 'https://sdtimes.com/wp-content/uploads/2016/07/0722.sdt-atlassian.png'
-        },
-        color: parseInt('0000FF', 16),
-        description: description,
-        url: `${changes.links.html.href}`
-      }
-    ]
-  };
-
-  try {
-    const response = await axios.post(url, data);
-  } catch (err) {
-    //console.log(err);
-  }
-  return data;
-};
-
-exports.sendCodeshipBuildNotification = async buildPayload => {
-  const build = buildPayload.build;
-  const status = build.status;
-  const url = config.discordWebhookUrl;
-
-  const data = {
-    embeds: [
-      {
-        title: `${build.project_full_name} build ${status}`,
-        author: {
-          name: 'Codeship',
-          icon_url: 'https://pbs.twimg.com/profile_images/884879047062323200/6PI7jOx0_400x400.jpg'
-        },
-        color: status === 'error'
-          ? parseInt('ff2100', 16)
-          : parseInt('00ba31', 16),
-        description: `Codeship build id ${build.build_id} has a status of ${build.status}.\n\n\[${build.short_commit_id}](<${build.commit_url}>)\ ${build.message} by ${build.committer}`,
-        url: `${build.build_url}`
-      }
-    ]
-  };
-
-  try {
-    const response = await axios.post(url, data);
-  } catch (err) {
-    //console.log(err);
-  }
-  return data;
-};
+sendErrorMessage = (message) => {
+  console.log('err');
+  message.channel.sendMessage('Something went wrong... sorry :(');
+}
 
 playSound = (message, soundName, voiceChannel) => {
-  if (availableSounds.indexOf(soundName) < 0)
-    return;
-  let channel = voiceChannel
-    ? voiceChannel
-    : message.member.voiceChannel;
+  if (availableSounds.indexOf(soundName) < 0) return;
+  let channel = voiceChannel ? voiceChannel : message.member.voiceChannel;
 
   if (channel && channel !== null) {
-    channel
-      .join()
-      .then(connection => {
-        const stream = connection.playFile(soundName + '.mp3', (err, intent) => {
-          if (err)
-            console.log(err);
-          console.log(intent);
-        });
-        stream.on('end', () => {
-          connection.disconnect();
-        });
+    channel.join().then(connection => {
+      const stream = connection.playFile(soundName + ".mp3", (err, intent) => {
+        if (err) console.log(err);
+        console.log(intent);
+        sendErrorMessage(message);
+        disconnect();
       });
+      stream.on("end", (reason) => {
+        console.log('end');
+        console.log(reason);
+        connection.disconnect();
+      });
+
+      // const stream = fs.createReadStream(`./${soundName}.mp3`)
+
+      // const dispatcher = connection.playStream(stream);
+      // const dispatcher = connection.playFile(`./${soundName}.mp3`);
+      // dispatcher.on('error', e => { console.log(e); sendErrorMessage(message) })
+      // dispatcher.on('end', (reason) => {
+      //   console.log('Stream ended because: ' + reason);
+      //   connection.disconnect()
+      // });
+      // dispatcher.on('debug', (info) => {
+      //   console.log('DEBUG: ' + info);
+      // });
+      // dispatcher.on('speaking', (speaking) => {
+      //   console.log('DEBUG: speaking ' + speaking);
+      // });
+      // dispatcher.on('start', () => {
+      //   console.log('DEBUG: started');
+      // });
+    })
+    .catch(err => console.log(err));
   }
 };
 
-client.on('ready', () => {
-  console.log('I am ready!');
-  client
-    .user
-    .setGame('with my penis');
+client.on("ready", () => {
+  console.log("I am ready!");
+  client.user.setActivity("with my penis", { type: 'PLAYING' });
 });
 
-client.on('message', async message => {
+client.on("message", async message => {
   if (message.author.username !== client.user.username) {
-    if (isParonityOrMdude(message.author.id) && message.content.indexOf('partyparrot') > -1) {
-      message
-        .channel
-        .send('<a:aussieparrot:432866431659540482>');
-    }
-    const prefix = message
-      .content
-      .charAt(0);
-    if (prefix !== config.prefix)
-      return;
-    const args = message
-      .content
+    const prefix = message.content.charAt(0);
+    if (prefix !== config.prefix) return;
+    const args = message.content
       .slice(1)
       .trim()
       .split(/ +/g);
-    const command = args
-      .shift()
-      .toLowerCase();
+    const command = args.shift().toLowerCase();
     const firstParam = args.shift();
     let voiceChannel = null;
-    if (firstParam === '-c' && isParonityOrMdude(message.author.id))
-      voiceChannel = message.guild.channels.find('name', args.join(' '));
+    if (firstParam === "-c" && isParonityOrMdude(message.author.id))
+      voiceChannel = message.guild.channels.find("name", args.join(" "));
 
-    if (command === 'ping') {
-      const m = await message
-        .channel
-        .send('Ping?');
-      m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+    if (command === "disconnect" || command === "stop") {
+      client.voiceConnections.forEach(connection => {
+        connection.disconnect();
+      });
     }
 
-    if (command === 'disconnect' || command === 'stop') {
-      client
-        .voiceConnections
-        .forEach(connection => {
-          connection.disconnect();
-        });
-    }
-
-    if (command === 'list') {
-      let list = '```Here is a list of all of the available commands:';
+    if (command === "list") {
+      let list = "```Here is a list of all of the available commands:";
       availableSounds = availableSounds.sort((a, b) => {
-        if (a < b)
-          return -1;
-        if (a > b)
-          return 1;
+        if (a < b) return -1;
+        if (a > b) return 1;
         return 0;
       });
       availableSounds.forEach((sound, index, array) => {
-        list += index != array.length - 1
-          ? `\n%${sound}`
-          : `\n%${sound}`;
+        list += index != array.length - 1 ? `\n%${sound}` : `\n%${sound}`;
       });
-      list += '```';
-      message
-        .member
-        .sendMessage(list);
+      list += "```";
+      message.member.sendMessage(list);
     }
 
     playSound(message, command, voiceChannel);
   }
 });
-
-isParonityOrMdude = id => {
-  return Number(id) == 103591900732231680 || Number(id) == 71386725435310080;
-};
 
 client.login(config.botKey);
